3c9e50f93b0547525fc6ade016b4aae3437e89b2
steppinlo <lo.stephaniec@gmail.com>
2016-02-03 21:27:56 -0500

    Initial Commit

---
+++ FlipClip/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  FlipClip
+//
+//  Created by Stephanie Lo on 2/3/16.
+//  Copyright © 2016 Stephanie Lo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
---
+++ FlipClipTests/FlipClipTests.m
@@ -0,0 +1,39 @@
+//
+//  FlipClipTests.m
+//  FlipClipTests
+//
+//  Created by Stephanie Lo on 2/3/16.
+//  Copyright © 2016 Stephanie Lo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FlipClipTests : XCTestCase
+
+@end
+
+@implementation FlipClipTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
